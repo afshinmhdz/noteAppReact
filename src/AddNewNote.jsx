2d6b8cbd3457bea8cd86c27bc70f0dfb39443aaf
afshinmhdz <afshinmhdz1906@gmail.com>
2024-05-12 22:36:05 +0330

    added two state for save user input value

--- src/AddNewNote.jsx
+++ src/AddNewNote.jsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 function AddNewNote() {
+const [title,setTitle]=useState("");
+const [description,setDescription]=useState("");
 
 const handleSubmit=(e)=>{
     e.preventDefault();
 }    
 
+const handleChange=(e)=>{
+    console.log(e.target.value);
+    setTitle(e.target.value);
+}
   return (
     <div className="add-new-note">
       <h2>Add New Note</h2>
       <form className="note-form" onSubmit={handleSubmit}>
-        <input type="text"  className="text-field" placeholder="Note title"/>
-        <input type="text"  className="text-field" placeholder="Note description"/>
+        <input value={title} onChange={(e)=>setTitle(e.target.value)} type="text"  className="text-field" placeholder="Note title"/>
+        <input value={description} onChange={(e)=>setDescription(e.target.value)} type="text"  className="text-field" placeholder="Note description"/>
         <button type="submit" className="btn btn--primary">Add New Note</button>
       </form>
     </div>
