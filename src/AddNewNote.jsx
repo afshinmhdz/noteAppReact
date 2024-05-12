dd1391e23cdffd2e2d6db2e7286876cc54f7f7ad
afshinmhdz <afshinmhdz1906@gmail.com>
2024-05-12 21:33:56 +0330

    add base tages in AddNewNote.jsx component

--- src/AddNewNote.jsx
+++ src/AddNewNote.jsx
@@ -1,4 +1,13 @@
-function AddNewNote(){
-    return(<div className="add-new-note">AddNewNote</div>)
+function AddNewNote() {
+  return (
+    <div className="add-new-note">
+      <h2>Add New Note</h2>
+      <form className="note-form">
+        <input type="text"  className="text-field" />
+        <input type="text"  className="text-field" />
+        <button className="btn btn--primary">Add New Note</button>
+      </form>
+    </div>
+  );
 }
-export default AddNewNote
+export default AddNewNote;
