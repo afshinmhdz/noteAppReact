c2420d946cc059048091363362732ed8f8934356
afshinmhdz <afshinmhdz1906@gmail.com>
2024-05-12 21:27:21 +0330

    added new component with name AddNewNote.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import AddNewNote from "./AddNewNote";
 import "./App.css";
 function App() {
   return <div className="container">
     <div className="note-header"></div>
     <div className="note-app">
-      <div className="add-new-note"></div>
+      <AddNewNote/>
       <div className="note-container"></div>
     </div>
     
