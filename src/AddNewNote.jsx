cedb3a0dfe292de6d6e87d78583d3b3a24d85c1d
afshinmhdz <afshinmhdz1906@gmail.com>
2024-05-12 21:44:22 +0330

    definition handleSubmit function for prevent to reload page

--- src/AddNewNote.jsx
+++ src/AddNewNote.jsx
@@ -1,11 +1,16 @@
 function AddNewNote() {
+
+const handleSubmit=(e)=>{
+    e.preventDefault();
+}    
+
   return (
     <div className="add-new-note">
       <h2>Add New Note</h2>
-      <form className="note-form">
-        <input type="text"  className="text-field" />
-        <input type="text"  className="text-field" />
-        <button className="btn btn--primary">Add New Note</button>
+      <form className="note-form" onSubmit={handleSubmit}>
+        <input type="text"  className="text-field" placeholder="Note title"/>
+        <input type="text"  className="text-field" placeholder="Note description"/>
+        <button type="submit" className="btn btn--primary">Add New Note</button>
       </form>
     </div>
   );
