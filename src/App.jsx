5375277881e0cb13562e791522a4c6d9858a5e87
afshinmhdz <afshinmhdz1906@gmail.com>
2024-05-13 21:33:11 +0330

    add new component NoteList.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import AddNewNote from "./AddNewNote";
 import "./App.css";
+import NoteList from "./NoteList";
 function App() {
   return <div className="container">
     <div className="note-header"></div>
     <div className="note-app">
       <AddNewNote/>
-      <div className="note-container"></div>
+      <div className="note-container">
+        <NoteList/>
+      </div>
     </div>
     
   </div>
---
+++ src/NoteList.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function NoteList(){
+    return(
+        <div>NoteList c</div>
+    )
+}
+
+export default NoteList
